46394820b61bae49202809c0dfe41e3c22b63ac1
brendan miller <brendanmiller.ca@gmail.com>
2019-06-04 19:27:00 -0500

    begin skeleton code

---
+++ src/Particle.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Particle{
+    glm::vec3 position;
+    glm::vec3 velocity;
+    
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <Partio.h>
+#include "SimulationDomain.h"
 
 int main() {
     std::cout << "apic" << std::endl;
+
+    // auto particleSet = Partio::create();
+    // particleSet->addAttribute("position",Partio::VECTOR,3);
+    // particleSet->addAttribute("velocity",Partio::VECTOR,3);
+    // particleSet->addAttribute("id",Partio::INT,1);
+
+    // particleSet->release();
 
     return 0;
 }
